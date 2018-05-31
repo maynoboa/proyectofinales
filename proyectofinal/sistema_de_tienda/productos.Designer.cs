505046ed9d3209f2fa3aba8b800a9e8caf86154a
Alumno <Alumno@WINDOWS7-PC>
2018-05-31 13:11:16 +0200

    Cambio especial

--- proyectofinal/sistema_de_tienda/productos.Designer.cs
+++ proyectofinal/sistema_de_tienda/productos.Designer.cs
@@ -29,10 +29,9 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.articulos = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,15 +42,6 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(707, 302);
             this.dataGridView1.TabIndex = 0;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(56, 367);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "productos";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
@@ -71,24 +61,24 @@
             this.button3.Text = "salir";
             this.button3.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // articulos
             // 
-            this.button4.Location = new System.Drawing.Point(570, 367);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.articulos.Location = new System.Drawing.Point(87, 367);
+            this.articulos.Name = "articulos";
+            this.articulos.Size = new System.Drawing.Size(75, 23);
+            this.articulos.TabIndex = 4;
+            this.articulos.Text = "Productos";
+            this.articulos.UseVisualStyleBackColor = true;
+            this.articulos.Click += new System.EventHandler(this.articulos_Click);
             // 
             // productos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button4);
+            this.Controls.Add(this.articulos);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "productos";
             this.Text = "productos";
@@ -100,9 +90,8 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button articulos;
     }
 }
--- proyectofinal/sistema_de_tienda/productos.cs
+++ proyectofinal/sistema_de_tienda/productos.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
 
 namespace sistema_de_tienda
 {
@@ -15,6 +16,11 @@ namespace sistema_de_tienda
         public productos()
         {
             InitializeComponent();
+        }
+
+        private void articulos_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
